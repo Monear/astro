c8abed947c117688236554527c3be1ad67a726b2
Monear <tyler.monette2@gmail.com>
2023-02-09 10:27:58 +0700

    modified:   astro.config.mjs 	modified:   package-lock.json 	modified:   package.json 	modified:   src/pages/about.astro 	modified:   src/pages/blog.astro 	new file:   src/pages/index1.astro 	new file:   src/pages/posts/post-1.md

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,24 @@
-import { defineConfig } from 'astro/config';
-
 // https://astro.build/config
 import netlify from "@astrojs/netlify/functions";
+import { defineConfig } from "astro/config";
+import storyblok from "@storyblok/astro";
 
 // https://astro.build/config
 export default defineConfig({
+  integrations: [
+    storyblok({
+      accessToken: "TVd6QLQct84fnJSH6cedyAtt",
+      components: {
+        page: "storyblok/Page",
+	      feature: "storyblok/Feature",
+	      grid: "storyblok/Grid",
+	      teaser: "storyblok/Teaser",
+      }
+    }),
+  ],
   output: "server",
   adapter: netlify()
 });
+
+
+
